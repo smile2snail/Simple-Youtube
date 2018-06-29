2b0c7624b4a6a5c9c3a024337314d6ae079338ca
Jiayin Song <jiayin.biz@gmail.com>
2018-06-29 14:11:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAmFcu07XBsIZeQOOk0aOdktgX7Uwxjo2Y'
+
+//Creat a new component. This component should produce
+//some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+     this.videoSearch('qingdao');
+  }
+
+videoSearch(term) {
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos:videos,
+      selectedVideo: videos[3]
+    });
+  });
+}
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) },300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+//Take this component's generated HTML and put it
+//on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
